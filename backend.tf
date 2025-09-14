77f435540866b7a5afd749ad9cc3794da1aea2d4
stephanjames <stephanjames055@gmail.com>
2025-09-14 00:27:05 +0000

    fixed bucket name in backend file

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-vpc"    # ton bucket S3 existant
+    bucket         = "terraform-state-vpc-eu-z1"    # ton bucket S3 existant
     key            = "vpc/terraform.tfstate"  # chemin du state
     region         = "eu-north-1"
     dynamodb_table = "terraform-locks"        # table DynamoDB pour le lock
