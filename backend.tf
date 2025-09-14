b8cff24d25a54bb7493ef09f876d906f2c33dce8
stephanjames <stephanjames055@gmail.com>
2025-09-14 00:43:37 +0000

    fixed bucket not created error

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-vpc-eu-z1"    # ton bucket S3 existant
     key            = "vpc/terraform.tfstate"  # chemin du state
     region         = "eu-north-1"
-    dynamodb_table = "terraform-locks"        # table DynamoDB pour le lock
+    dynamodb_table = "terraform-locks"        # table DynamoDB pour le locks
     encrypt        = true
   }
 }
